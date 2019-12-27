a470fd75fe3865fa7e6900a856de0e0915dd6970
kaname.i <lab.luv.love@i.softbank.jp>
2019-12-27 09:56:40 +0900

    first commit

---
+++ src/sample/random.java
@@ -0,0 +1,51 @@
+package sample;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class random {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	Random rand = new Random();
+
+	System.out.println("----Random NUmber game----");
+
+	int randomNumber = rand.nextInt(100) + 1;
+	int guess;
+
+	for(int i = 1; i <=10; i++)
+	{
+		System.out.println("Enter guess number " + i + ":" );
+		guess = input.nextInt();
+
+		if(guess == randomNumber)
+		{
+			System.out.println("Bingo");
+			System.out.println("the random number was " + randomNumber);
+		}
+			else if(guess > randomNumber)
+			{
+				System.out.println("too high");
+				if(i == 10)
+				{
+					System.out.println("game over");
+					System.out.println("the random number was " + randomNumber);
+			}
+			}
+				else if(guess < randomNumber)
+				{
+				System.out.println("too low");
+				if(i == 10)
+				{
+					System.out.println("game over");
+					System.out.println("the random number was" + randomNumber);
+				}
+
+		}
+	}
+
+	}
+	}
+
+
