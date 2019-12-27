8123e0abd7632eaec85379a7bf707aad4d95436d
kaname.i <lab.luv.love@i.softbank.jp>
2019-12-28 00:09:08 +0900

    2nd commit

--- src/sample/random.java
+++ src/sample/random.java
@@ -6,46 +6,32 @@ import java.util.Scanner;
 public class random {
 
 	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	Random rand = new Random();
-
-	System.out.println("----Random NUmber game----");
-
-	int randomNumber = rand.nextInt(100) + 1;
-	int guess;
-
-	for(int i = 1; i <=10; i++)
-	{
-		System.out.println("Enter guess number " + i + ":" );
-		guess = input.nextInt();
-
-		if(guess == randomNumber)
-		{
-			System.out.println("Bingo");
-			System.out.println("the random number was " + randomNumber);
-		}
-			else if(guess > randomNumber)
-			{
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
+		System.out.println("----Random NUmber game----");
+		int randomNumber = rand.nextInt(100) + 1;
+		int guess;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Enter guess number " + i + ":");
+			guess = input.nextInt();
+			if (guess == randomNumber) {
+				System.out.println("Bingo");
+				System.out.println("the random number was " + randomNumber);
+			} else if (guess > randomNumber) {
 				System.out.println("too high");
-				if(i == 10)
-				{
+				if (i == 10) {
 					System.out.println("game over");
 					System.out.println("the random number was " + randomNumber);
-			}
-			}
-				else if(guess < randomNumber)
-				{
+				}
+			} else if (guess < randomNumber) {
 				System.out.println("too low");
-				if(i == 10)
-				{
+				if (i == 10) {
 					System.out.println("game over");
 					System.out.println("the random number was" + randomNumber);
 				}
 
+			}
 		}
-	}
 
 	}
-	}
-
-
+}
